02c733c207e37e17b9ef07774f0e1ca3b09ce49c
Christian Autermann <c.autermann@52north.org>
2026-01-27 19:21:38 +0100

    bump dependency versions

--- src/main/java/org/n52/jackson/datatype/jts/Type.java
+++ src/main/java/org/n52/jackson/datatype/jts/Type.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2025 52°North Spatial Information Research GmbH
+ * Copyright 2019-2026 52°North Spatial Information Research GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
